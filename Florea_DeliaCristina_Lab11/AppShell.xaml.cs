f66eac45e064dac9031cfcf4a7c2ab6f51f5dad7
CristinaaFlorea <floreacristina30@yahoo.ro>
2022-12-23 01:39:03 +0200

    Add project files.

---
+++ Florea_DeliaCristina_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Florea_DeliaCristina_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
